3c9e11bcc28f029f7295248b398f814a709afced
Adrian Phuong <61097159+adrianphuong@users.noreply.github.com>
2023-11-20 18:11:57 -0800

    Update HandleAPI.js

--- src/utils/HandleAPI.js
+++ src/utils/HandleAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = "http://localhost:4000"
+const baseURL = "https://fullstack-todo-app-backendnode.onrender.com"
 
 const getAllToDo = (setToDo) => {
     axios
